9d38a7e6fad6554cf8b4a79cac071d856b1e0f17
Mottaya Samy. K <145455585+mottayasamy@users.noreply.github.com>
2024-10-04 17:13:02 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Retrieve the username from the form
+            username = form.cleaned_data['username']
+            
+            # Pass the username to the success template
+            return render(request, 'registration_success.html', {'username': username})
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
